f66eaaed3992acd0e92ed6835f6c4df761f471c6
Mobile Application Course <44363418+mobilecourse2@users.noreply.github.com>
2018-12-04 10:12:35 +0500

    Add files via upload

---
+++ GuiStructure.java
@@ -0,0 +1,11 @@
+ 
+package guistructure;
+ 
+public class GuiStructure {
+
+  
+    public static void main(String[] args) {
+         
+    }
+    
+}
---
+++ GuiWindowExample.java
@@ -0,0 +1,7 @@
+ 
+package guistructure;
+
+ 
+public class GuiWindowExample {
+    
+}
