8bb8316131a64c16d00847b510a00ed85425c26e
Mobile Application Course <44363418+mobilecourse2@users.noreply.github.com>
2018-12-04 10:15:21 +0500

    Add files via upload

--- GuiStructure.java
+++ GuiStructure.java
@@ -1,11 +1,50 @@
  
-package guistructure;
+package guiwindowexample;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
  
-public class GuiStructure {
-
-  
-    public static void main(String[] args) {
-         
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+ 
+public class GuiStructure extends WindowAdapter{
+    
+    
+        GuiStructure(){
+    
+        initGui();
+        
     }
+    
+    public void initGui(){
+    
+        JFrame window = new JFrame("GuiWindowExample");
+        Container container = window.getContentPane();
+        
+        container.setLayout(new FlowLayout());
+        
+        JButton button1 = new JButton("Button");
+    
+         
+        window.addWindowListener(this);
+        window.setSize(200,200);
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+  
+    @Override
+    public void windowClosing(WindowEvent e) {
+    
+        JOptionPane.showMessageDialog(null,"Window Closed");
+            System.exit(0);
+        
+    }
+
+    
+
     
 }
--- GuiWindowExample.java
+++ GuiWindowExample.java
@@ -1,7 +1,13 @@
  
-package guistructure;
+package guiwindowexample;
 
  
 public class GuiWindowExample {
+
+     
+    public static void main(String[] args) {
+        GuiStructure g = new GuiStructure();
+         
+    }
     
 }
